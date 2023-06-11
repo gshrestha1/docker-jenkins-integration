852cc5023f63401436980240f45bef2e70eda466
Gopal Shrestha <gshresth@dollargeneral.com>
2023-06-11 14:33:42 -0400

    First commit

---
+++ src/test/java/com/dollar/general/dockerjenkinsintegration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollar.general.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
